ec0b527bd5c270b368e563bd07a0811e37c06498
free4one <free4one@gmail.com>
2019-02-18 16:43:46 +0900

    TestSuites1 changed.

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -11,21 +11,9 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>9137b763-a9f8-4a94-8f5a-0dd65d5802f1</testSuiteGuid>
    <testCaseLink>
-      <guid>d9d3a233-fc7a-477c-a312-444386cab36f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/GetBankDetails</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>573f0652-8850-4047-907b-9c643f3a6dcf</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ListUsersTest</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>e7e9e7b1-ff12-4c8f-82fc-d3183b555e81</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
